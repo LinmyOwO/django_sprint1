f9e20736023f054e7f9893a0fc904ee14d0551a1
Grisha Shevchuk <grisha_shev2002@mail.ru>
2024-12-03 03:18:45 +0700

    Finally working version

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -47,14 +47,14 @@ posts = [
 
 def index(request):
     context = {'posts': posts}
-    return render(request, 'index.html', context)
+    return render(request, 'blog/index.html', context)
 
 
 def post_detail(request, id):
     context = {'post': posts[id]}
-    return render(request, 'detail.html', context)
+    return render(request, 'blog/detail.html', context)
 
 
 def category_post(request, category_slug):
     context = {'category_slug': category_slug}
-    return render(request, 'category.html', context)
+    return render(request, 'blog/category.html', context)
